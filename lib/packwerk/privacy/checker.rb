af3d1a97527c65c5bc5aa5d472a393fa58fcd41d
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-12-19 09:35:23 -0500

    Remove functionality from privacy checker

--- lib/packwerk/privacy/checker.rb
+++ lib/packwerk/privacy/checker.rb
@@ -27,12 +27,7 @@ module Packwerk
         return false if privacy_package.public_path?(reference.constant.location)
 
         privacy_option = privacy_package.enforce_privacy
-        return false if enforcement_disabled?(privacy_option)
-
-        return false unless privacy_option == true || privacy_option == 'strict' ||
-                            explicitly_private_constant?(reference.constant, explicitly_private_constants: T.unsafe(privacy_option))
-
-        true
+        !enforcement_disabled?(privacy_option)
       end
 
       sig do
@@ -64,18 +59,6 @@ module Packwerk
       end
 
       private
-
-      sig do
-        params(
-          constant: ConstantContext,
-          explicitly_private_constants: T::Array[String]
-        ).returns(T::Boolean)
-      end
-      def explicitly_private_constant?(constant, explicitly_private_constants:)
-        explicitly_private_constants.include?(constant.name) ||
-          # nested constants
-          explicitly_private_constants.any? { |epc| constant.name.start_with?("#{epc}::") }
-      end
 
       sig do
         params(privacy_option: T.nilable(T.any(T::Boolean, String, T::Array[String])))
